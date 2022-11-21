7675f095c02e378e4575728bdff5964e33f24694
Julsgaard <kjulsg21@student.aau.dk>
2022-11-21 12:07:28 +0100

    Simpler average color

--- Labraries/Average_Tile_Color.py
+++ Labraries/Average_Tile_Color.py
@@ -1,126 +1,57 @@
 import numpy as np
-import cv2 as cv
-
-"""
-Slice_IMG makes 5 times 5 slices of the input image
-"""
-
-
-def slice_IMG(input_image):
-    output = []
-    for y in range(0, input_image.shape[0], int(input_image.shape[0] / 5)):
-        yLine = []
-        for x in range(0, input_image.shape[1], int(input_image.shape[1] / 5)):
-            slice = input_image[y: y + int(input_image.shape[0] / 5), x: x + int(input_image.shape[1] / 5)]
-            yLine.append(slice)
-        output.append(yLine)
-
-    return output
 
 
 def slice_img(img):
-    M = img.shape[0] // 5
-    N = img.shape[1] // 5
+    img_width, img_height = img.shape[0], img.shape[1]
+    slice_width, slice_height = img_width / 5, img_height / 5
+    width, height = int(slice_width), int(slice_height)
 
-    print(M, N)
+    slice_list = []
 
-    tiles = [img[x:x + M, y:y + N] for x in range(0, img.shape[0], M) for y in range(0, img.shape[1], N)]
+    for y in range(5):
+        for x in range(5):
+            slice = img[width * y: width * (y + 1), height * x:height * (x + 1)]
+            slice_list.append(slice)
 
-    print(tiles)
-
-    cv.imshow("tilesss", tiles)
-    cv.waitKey(0)
-
-    return output
-
-
-# TODO: Make simpler
-def get_dominant_colour(tile):
-    tile_colors = []
-    for i in range(len(tile)):
-        for j in range(len(tile[i])):
-            data = np.reshape(tile[i][j], (-1, 3))
-            data = np.float32(data)
-
-            criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-            flags = cv.KMEANS_RANDOM_CENTERS
-            compactness, labels, centers = cv.kmeans(data, 1, None, criteria, 10, flags)
-
-            color = centers[0].astype(np.int32)
-            # print(f'Dominant color is: bgr({color})')
-            tile_colors.append(color)
-
-    return tile_colors
+    return slice_list
 
 
-def get_average_color(img: np.ndarray):
-    # print(len(tile[1]))
-    width, height = img.shape[0], img.shape[1]
+def get_average_color(tile_colors):
+    blue_list = []
+    red_list = []
+    green_list = []
+    for i in range(len(tile_colors)):
+        width, height = tile_colors[i].shape[0], tile_colors[i].shape[1]
 
-    blue, green, red = 0, 0, 0
-    for x in range(width):
-        for y in range(height):
-            # print(f"{x},{y}")
-            blue += img[x, y, 0]
-            green += img[x, y, 1]
-            red += img[x, y, 2]
+        blue, green, red = 0, 0, 0
 
-    # Calculates the average color
-    blue /= width * height
-    green /= width * height
-    red /= width * height
-    for x in range(width):
-        for y in range(height):
-            img[x, y, 0] = blue
-            img[x, y, 1] = green
-            img[x, y, 2] = red
+        for x in range(width):
+            for y in range(height):
+                # print(f"{x},{y}")
+                blue += tile_colors[i][x, y, 0]
+                green += tile_colors[i][x, y, 1]
+                red += tile_colors[i][x, y, 2]
 
-    print(blue, red, green)
-    return blue, red, green
+        # Calculates the average color
+        blue /= width * height
+        green /= width * height
+        red /= width * height
 
+        blue_list.append(blue)
+        green_list.append(green)
+        red_list.append(red)
 
-def set_tile_color(tile_colors):
-    matrix1D = np.zeros((25, 1, 3), dtype=np.uint8)
-
-    for i in range(len(matrix1D)):
-        matrix1D[i, 0, 0] = tile_colors[i][0]
-        matrix1D[i, 0, 1] = tile_colors[i][1]
-        matrix1D[i, 0, 2] = tile_colors[i][2]
-
-    # Getting list with hsv for tiles
-    output = []
-    tile_colors_to_hsv = cv.cvtColor(matrix1D, cv.COLOR_BGR2HSV)
-    # print(tile_colors_to_hsv)
-
-    matrix2D = np.reshape(matrix1D, (5, 5, 3))
-
-    return matrix2D
+    return blue_list, green_list, red_list
 
 
-"""
-Creating custom image the same size as input image, which is 5 times 5 + 2 in each row 
-if we want to use a 4 connectivity grassfire later on to avoid getting the " out of bounce" message 
-"""
+def set_tile_color(blue_list, green_list, red_list):
+    matrix = np.zeros((25, 1, 3), dtype=np.uint8)
 
-customIMG = np.array([[0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0]], dtype=np.uint8)
+    for i in range(len(matrix)):
+        matrix[i, 0, 0] = blue_list[i]
+        matrix[i, 0, 1] = green_list[i]
+        matrix[i, 0, 2] = red_list[i]
 
-# customIMG = np.zeros((7, 7), dtype=int)
+    matrix_2d = np.reshape(matrix, (5, 5, 3))
 
-
-"""
-We assign numbers to each possible field variation
-"""
-
-crownField = 1
-mine = 2
-water = 3
-Desert = 4
-grassLand = 5
-woodLand = 6
-dirtLand = 7
+    return matrix_2d
--- Main.py
+++ Main.py
@@ -1,41 +1,32 @@
 import cv2
 
-from Labraries import Non_Max_Suppression, Average_Tile_Color, Find_Crowns, Tile_Threshold, Connect_Tiles, Crown_Tile, Count_Points
+from Labraries import Non_Max_Suppression, Average_Tile_Color, Find_Crowns, Tile_Threshold, Connect_Tiles, Crown_Tile, \
+    Count_Points
 
 image = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/20.jpg")
 
-
-#Template matching and making bounding boxes around the crowns
+# Template matching and making bounding boxes around the crowns
 boxes = Find_Crowns.template_matching(image)
 
-#Non-max-suppression so the bounding boxes do not overlap
+# Non-max-suppression so the bounding boxes do not overlap
 NMS_Boxes = Non_Max_Suppression.non_max_suppression(boxes)
 
-#Getting the center coordinates of the bounding box
+# Getting the center coordinates of the bounding box
 box_center_coordinates = Non_Max_Suppression.get_box_center(NMS_Boxes)
 
-#Drawing the center coordinates on a black image
+# Drawing the center coordinates on a black image
 box_coordinates_image, box_image = Non_Max_Suppression.draw_box_coordinates(image, box_center_coordinates)
 
-#Converts the crown coordinates to a 5x5 numpy array
+# Converts the crown coordinates to a 5x5 numpy array
 crown_tile = Crown_Tile.find_crown_tile(box_center_coordinates, image.shape[0], image.shape[1])
 
+#Slices the image into 25 pieces
+tiles = Average_Tile_Color.slice_img(image)
 
-tiles = Average_Tile_Color.slice_IMG(image)
+# TODO: Maybe get median rgb instead of average
+blue_list, green_list, red_list = Average_Tile_Color.get_average_color(tiles)
 
-#TODO: Get this one to work as a numpy array
-#tiles = Average_Tile_Color.slice_img(image)
-
-
-#TODO: Simpler version of get_dominant_colour
-#TODO: Maybe get median rgb instead of average
-
-tiles_dominant_color = Average_Tile_Color.get_dominant_colour(tiles)
-
-#Average_Tile_Color.get_average_color(tiles)
-
-
-combined_tiles = Average_Tile_Color.set_tile_color(tiles_dominant_color)
+combined_tiles = Average_Tile_Color.set_tile_color(blue_list, green_list, red_list)
 
 thresh_grass, thresh_woods, thresh_water, thresh_desert, thresh_dirt, thresh_mine = \
     Tile_Threshold.tile_threshold(combined_tiles)
@@ -43,23 +34,18 @@ thresh_grass, thresh_woods, thresh_water, thresh_desert, thresh_dirt, thresh_min
 connected_grass, connected_woods, connected_water, connected_desert, connected_dirt, connected_mine = \
     Connect_Tiles.connect_tile(thresh_grass, thresh_woods, thresh_water, thresh_desert, thresh_dirt, thresh_mine)
 
-
-total_points = Count_Points.connected_terrains\
+total_points = Count_Points.connected_terrains \
     (crown_tile, connected_grass, connected_woods, connected_water, connected_desert, connected_dirt, connected_mine)
 
-#Count_Points.count_points()
-
-#print(tiles_dominant_color)
 
 print(f"Crown tiles: \n {crown_tile}")
-
 print(f"Total Points: {total_points}")
 
 cv2.imshow("combined tiles", combined_tiles)
-#cv2.imshow("tile", tiles[0][0])
-#cv2.imshow("average_color", tiles_average_color[])
-cv2.imshow("image", image)
-#cv2.imshow("box_center_coordinates", box_coordinates_image)
-#cv2.imshow("box_image", box_image)
+# cv2.imshow("tiles_dominant_color", tiles_dominant_color)
+# cv2.imshow("tile", tiles[0][0])
+# cv2.imshow("average_color", tiles_average_color[])
+#cv2.imshow("image", image)
+# cv2.imshow("box_center_coordinates", box_coordinates_image)
+cv2.imshow("box_image", box_image)
 cv2.waitKey(0)
-
